967479dc2e5df9ed4a478576ca283c41c931ac31
Riley Douglas <rileydouglas@outlook.com>
2022-11-15 19:23:27 +1300

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { optimizeImports } from 'carbon-preprocess-svelte';
 import preprocess from 'svelte-preprocess';
 
