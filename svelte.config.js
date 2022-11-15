651c10cf3a4db7a37b4859ff146fcf1c9fa97004
Riley Douglas <rileydouglas@outlook.com>
2022-11-15 16:32:35 +1300

    dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess(), optimizeImports()],
 
 	kit: {
 		adapter: adapter()
